f1c5a45b5306f5abc0b8d132da53ff51295007a3
Jayden <jayden6849@gmail.com>
2025-05-29 20:04:34 +0900

    [Jayden] 1st commit

---
+++ member-module/src/main/java/com/jd/member_module/home/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.jd.member_module.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"","/","/home"})
+    public String home() {
+        return "home";
+    }
+
+}
---
+++ member-module/src/test/java/com/jd/member_module/MemberModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.member_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
